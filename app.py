3cc93320908848b2cc7e56503ab81a8c81b646c8
Yan Liang <yanliang2345@outlook.com>
2021-12-30 21:40:28 +0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import os
 from werkzeug.utils import secure_filename
 from flask import Flask,flash,request,redirect,send_file,render_template
-UPLOAD_FOLDER = 'uploads/'
+UPLOAD_FOLDER = '/data/'
 #app = Flask(__name__)
 app = Flask(__name__, template_folder='templates')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 # Upload API
 @app.route('/uploadfile', methods=['GET', 'POST'])
 def upload_file():
@@ -26,13 +27,18 @@ def upload_file():
       #send file name as parameter to downlad
             return redirect('/downloadfile/'+ filename)
     return render_template('upload_file.html')
+
 # Download API
 @app.route("/downloadfile/<filename>", methods = ['GET'])
 def download_file(filename):
     return render_template('download.html',value=filename)
+
 @app.route('/return-files/<filename>')
 def return_files_tut(filename):
     file_path = UPLOAD_FOLDER + filename
     return send_file(file_path, as_attachment=True, attachment_filename='')
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(
+        host='0.0.0.0',
+        port = 9711)
